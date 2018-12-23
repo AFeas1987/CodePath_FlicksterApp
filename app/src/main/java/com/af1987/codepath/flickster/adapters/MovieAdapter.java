43f182cbe09e2638902cdd7df5e4d30611ae4a40
Alex F <majestic254@gmail.com>
2018-12-23 14:08:50 -0500

    UI updated significantly

--- app/src/main/java/com/af1987/codepath/flickster/adapters/MovieAdapter.java
+++ app/src/main/java/com/af1987/codepath/flickster/adapters/MovieAdapter.java
@@ -2,7 +2,12 @@ package com.af1987.codepath.flickster.adapters;
 
 import android.content.Context;
 import android.content.res.Configuration;
+import android.graphics.Bitmap;
+import android.graphics.Paint;
+import android.graphics.drawable.BitmapDrawable;
+import android.graphics.drawable.Drawable;
 import android.support.annotation.NonNull;
+import android.support.annotation.Nullable;
 import android.support.v7.widget.RecyclerView;
 import android.view.LayoutInflater;
 import android.view.View;
@@ -13,7 +18,11 @@ import android.widget.TextView;
 import com.af1987.codepath.flickster.R;
 import com.af1987.codepath.flickster.models.Movie;
 import com.bumptech.glide.Glide;
+import com.bumptech.glide.load.resource.bitmap.RoundedCorners;
 import com.bumptech.glide.request.RequestOptions;
+import com.bumptech.glide.request.target.SimpleTarget;
+import com.bumptech.glide.request.target.Target;
+import com.bumptech.glide.request.transition.Transition;
 
 import java.util.List;
 
@@ -30,24 +39,39 @@ public class MovieAdapter extends RecyclerView.Adapter<MovieAdapter.ViewHolder>{
     @NonNull
     @Override
     public ViewHolder onCreateViewHolder(@NonNull ViewGroup viewGroup, int i) {
-        return new ViewHolder(LayoutInflater.from(context)
-                .inflate(R.layout.rv_movie, viewGroup, false));
+            return new ViewHolder(LayoutInflater.from(context)
+                    .inflate(R.layout.rv_movie_plain, viewGroup, false));
     }
 
     @Override
-    public void onBindViewHolder(@NonNull ViewHolder viewHolder, int i) {
-        viewHolder.bind(movies.get(i));
-    }
+    public void onBindViewHolder(@NonNull ViewHolder holder, int i) {holder.bind(movies.get(i));}
 
     @Override
     public int getItemCount() {
         return movies.size();
     }
 
+    @Override
+    public int getItemViewType(int position) {
+        if (movies.get(position).getPopularity() > 200)
+            return 1;
+        else
+            return 0;
+    }
 
     class ViewHolder extends RecyclerView.ViewHolder {
         TextView tvTitle, tvDesc;
         ImageView ivPoster, ivBackdrop;
+        View itemView;
+
+        private SimpleTarget<Drawable> target = new SimpleTarget<Drawable>() {
+            @Override
+            public void onResourceReady(@NonNull Drawable resource,
+                        @Nullable Transition<? super Drawable> transition) {
+                itemView.setBackground(resource);
+                itemView.getBackground().setAlpha(100);
+            }
+        };
 
         ViewHolder(@NonNull View itemView) {
             super(itemView);
@@ -55,21 +79,32 @@ public class MovieAdapter extends RecyclerView.Adapter<MovieAdapter.ViewHolder>{
             tvDesc = itemView.findViewById(R.id.tvDesc);
             ivPoster = itemView.findViewById(R.id.ivPoster);
             ivBackdrop = itemView.findViewById(R.id.ivBackdrop);
+            this.itemView = itemView;
         }
 
         void bind(Movie movie) {
             tvTitle.setText(movie.getTitle());
+            tvTitle.setPaintFlags(tvTitle.getPaintFlags() | Paint.UNDERLINE_TEXT_FLAG);
             tvDesc.setText(movie.getDesc());
-            if (context.getResources().getConfiguration().orientation == Configuration.ORIENTATION_PORTRAIT)
+            if (context.getResources().getConfiguration()
+                    .orientation == Configuration.ORIENTATION_PORTRAIT)
                 Glide.with(context).load(movie.getPosterPath())
-                        .apply(new RequestOptions().placeholder(R.drawable.ic_launcher_background)
-                        .override(200, 300))
+                        .apply(new RequestOptions().placeholder(R.drawable.ic_launcher_foreground)
+                        .override(480, 720)
+                        .transform(new RoundedCorners(8)))
                         .into(ivPoster);
             else
                 Glide.with(context).load(movie.getBackdropPath())
-                        .apply(new RequestOptions().placeholder(R.drawable.ic_launcher_background)
-                        .override(400, 200))
+                        .apply(new RequestOptions().placeholder(R.drawable.ic_launcher_foreground)
+                        .override(800, 400)
+                        .transform(new RoundedCorners(8)))
                         .into(ivBackdrop);
+            if (movie.getPopularity() > 200) {
+                Glide.with(context).load(movie.getBackdropPath())
+                        .apply(new RequestOptions()
+                        .transform(new RoundedCorners(16)))
+                        .into(target);
+            }
         }
     }
 }
--- app/src/main/java/com/af1987/codepath/flickster/models/Movie.java
+++ app/src/main/java/com/af1987/codepath/flickster/models/Movie.java
@@ -11,17 +11,19 @@ public class Movie {
 
     private String posterPath, backdropPath;
     private String title, desc;
+    private int popularity;
 
     private Movie(JSONObject json) throws JSONException {
         posterPath = json.getString("poster_path");
         title = json.getString("title");
         desc = json.getString("overview");
         backdropPath = json.getString("backdrop_path");
+        popularity = json.getInt("popularity");
     }
 
     public String getTitle() {return title;}
     public String getDesc() {return desc;}
-
+    public int getPopularity() {return popularity;}
 
     public String getPosterPath() {
         return String.format("https://image.tmdb.org/t/p/w342/%s", posterPath);
@@ -30,7 +32,6 @@ public class Movie {
     public String getBackdropPath() {
         return String.format("https://image.tmdb.org/t/p/w342/%s", backdropPath);
     }
-
 
     public static List<Movie> fromJSONArray (JSONArray array) throws JSONException{
         List<Movie> movies = new ArrayList<>();
